6853c3e84a87036e6bba66e725bd8f15d7da8282
minh khoa <p3t3r276@pop-os.localdomain>
2020-05-15 18:42:27 +0700

    link to Book Details

--- src/apps/Book/BookList/BookListComponent.tsx
+++ src/apps/Book/BookList/BookListComponent.tsx
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react';
 import {FlatList, TouchableWithoutFeedback} from 'react-native';
 import {ListItem} from 'react-native-elements';
 import firestore from '@react-native-firebase/firestore';
+import {useNavigation} from '@react-navigation/native';
 
 import {Book} from '../../../../types/Book';
 
 export const BookListComponent = () => {
   const [bookList, setBookList] = useState<Book[]>([]);
 
+  const navigation = useNavigation();
   useEffect(() => {
     const fetchBook = async () => {
       setBookList(await getBooks());
@@ -17,7 +19,8 @@ export const BookListComponent = () => {
 
   const keyExtractor = (item: any) => item.id;
   const renderItem = ({item}: {item: Book}) => (
-    <TouchableWithoutFeedback onPress={() => console.log(item.name)}>
+    <TouchableWithoutFeedback
+      onPress={() => navigation.navigate('BookDetails')}>
       <ListItem key={item.id} title={item.name} bottomDivider chevron />
     </TouchableWithoutFeedback>
   );
